a594261343693de0909ae97faa60ef6397e17685
garyfoo88 <reid9000200020002000@gmail.com>
2021-06-10 09:16:13 +0800

    added env var

--- pages/search.js
+++ pages/search.js
@@ -4,7 +4,6 @@ import { useRouter } from "next/router";
 import React from "react";
 import Header from "../components/Header";
 import SearchResults from "../components/SearchResults";
-import { API_KEY, CONTEXT_KEY } from "../keys";
 import Response from "../Response";
 
 function Search({ results }) {
@@ -32,7 +31,7 @@ export async function getServerSideProps(context) {
     ? Response
     : await axios
         .get(
-          `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+          `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
         )
         .then(({ data }) => {
           return data;
